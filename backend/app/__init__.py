0d058d94a67718e456eabcf6e0940ec5aa9f0e41
masharia47th <glenmavricc@gmail.com>
2025-08-13 12:27:55 +0300

    added posgresql, run and tested authentication successfully

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    migrate.init_app(app, db)
     
     from app.models import user
     from app.routes.auth import auth_bp
